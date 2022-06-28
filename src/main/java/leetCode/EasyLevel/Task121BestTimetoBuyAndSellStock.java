5303df0e3a886acead661a897faca3aa089f3535
viet <v.nguen@amg-bs.ru>
2022-06-28 16:52:20 +0300

    add new task best time to buy and sell Stock

---
+++ src/main/java/leetCode/EasyLevel/Task121BestTimetoBuyAndSellStock.java
@@ -0,0 +1,57 @@
+package leetCode.EasyLevel;
+/**
+ * You are given an array prices where prices[i] is the price of a given stock on the ith day.
+ *
+ * You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+ *
+ * Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: prices = [7,1,5,3,6,4]
+ * Output: 5
+ * Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+ * Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+ * Example 2:
+ *
+ * Input: prices = [7,6,4,3,1]
+ * Output: 0
+ * Explanation: In this case, no transactions are done and the max profit = 0.
+ *
+ */
+public class Task121BestTimetoBuyAndSellStock {
+
+    public int getProfitWithOutMathFunction(int[] prices){
+        int buy = Integer.MAX_VALUE;
+        int sell = 0;
+
+        for(int i = 0; i < prices.length; i++){
+            if(prices[i] < buy){
+                buy = prices[i];
+            }
+
+            int profit = prices[i] - buy;
+
+            if(sell < profit){
+                sell = profit;
+            }
+        }
+
+
+        return sell;
+    }
+
+    public int getProfitWithMathFunction(int[] prices){
+        int buy = Integer.MAX_VALUE;
+        int sell = 0;
+
+        for(int i = 0; i < prices.length; i++){
+            buy = Math.min(buy, prices[i]);
+            sell = Math.max(sell, prices[i] - buy);
+        }
+
+        return sell;
+    }
+}
---
+++ src/test/java/leetCode/EasyLevel/Task121BestTimetoBuyAndSellStockTest.java
@@ -0,0 +1,32 @@
+package leetCode.EasyLevel;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Task121BestTimetoBuyAndSellStockTest {
+
+    private final Task121BestTimetoBuyAndSellStock sellStock = new Task121BestTimetoBuyAndSellStock();
+    private final int[] prices1 = new int[]{7,1,5,3,6,4};
+    private final int[] prices2 = new int[]{7,6,4,3,1};
+
+    @Test
+    void getProfitWithOutMathFunction() {
+        assertEquals(5, sellStock.getProfitWithMathFunction(prices1));
+    }
+
+    @Test
+    void getProfitWithMathFunction() {
+        assertEquals(5, sellStock.getProfitWithOutMathFunction(prices1));
+    }
+
+    @Test
+    void doNotgetProfitWithOutMathFunction() {
+        assertEquals(0, sellStock.getProfitWithMathFunction(prices2));
+    }
+
+    @Test
+    void doNotgetProfitWithMathFunction() {
+        assertEquals(0, sellStock.getProfitWithOutMathFunction(prices2));
+    }
+}
